b5e2f818c09fa3091782c4de463148a20f1e895b
nothingrotf <enderbr4@gmail.com>
2024-03-12 16:18:47 -0300

    chore: add eslint rule to enforce the usage of @ instead of relative imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'no-relative-import-paths'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-relative-import-paths/no-relative-import-paths': [
+      'error',
+      {
+        allowSameFolder: true,
+        prefix: '@',
+        rootDir: 'src'
+      }
+    ],
   },
 }
